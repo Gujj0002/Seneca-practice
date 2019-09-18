583a6a87ad0b67879335f2609a777945303f6921
Alpesh Gujjar <gujj0002@algonquinlive.com>
2019-09-18 18:23:21 -0400

    Plugin

---
+++ plugin.js
@@ -0,0 +1,12 @@
+module.exports = function math(options) {
+    this.add({
+        role: 'math',
+        cmd: 'sum'
+    }, (msg, respond) => {
+        var sum = msg.left + msg.right
+        respond(null, {
+            answer: sum
+        })
+    })
+    return 'operations'
+}
